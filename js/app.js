1da9829334f4077e63761cc4885f2ec0bebda12c
Yasamato <agent77326@gmail.com>
2021-01-10 15:09:31 +0100

    Reworked bootstrap design (#2)
    * Reworked bootstrap design
    
    * Adjust to previous changes on master
    
    * Correct wrong color-scheme
    
    * Table color-scheme corrected
    
    * Update .gitignore
    
    * Make it dark again
    
    * Make alert visable again
    
    * Delete Dockerfile
    
    * Make search und nav-tab in the same row
    
    * Adjust colors
    
    * Adjust search-border
    
    * Make nav and footer dark
    
    * Added noscript-info

--- js/app.js
+++ js/app.js
@@ -290,34 +290,19 @@ window.onload = e => {
 	      parsedData[key] = entries
 	    })
 
-	    // MANGA SITES ------------------------------
-	    const mangaTable = $('#mangaTable').DataTable(getMangaTableOptions([...parsedData.englishMangaSites, ...parsedData.foreignMangaSites]))
-	    const scansTable = $('#scansTable').DataTable(getMangaTableOptions([...parsedData.englishMangaScans, ...parsedData.foreignMangaScans]))
-	    // Handles using a single search bar for multiple tables
-	    $('#mangaTableSearch').on('keyup click', function () {
-	      mangaTable.tables().search($(this).val()).draw()
-	      scansTable.tables().search($(this).val()).draw()
-	    })
-
 	    // ANIME SITES ------------------------------
 	    const animeEnglishTable = $('#animeEnglishTable').DataTable(getAnimeTableOptions(parsedData.englishAnimeSites))
 	    const animeForeignTable = $('#animeForeignTable').DataTable(getAnimeTableOptions(parsedData.foreignAnimeSites))
 	    const animeDownloadTable = $('#animeDownloadTable').DataTable(getAnimeTableOptions(parsedData.animeDownloadSites))
-	    // Handles using a single search bar for multiple tables
-	    $('#animeTableSearch').on('keyup click', function () {
-	      animeEnglishTable.tables().search($(this).val()).draw()
-	      animeForeignTable.tables().search($(this).val()).draw()
-	      animeDownloadTable.tables().search($(this).val()).draw()
-	    })
+
+        // MANGA SITES ------------------------------
+        const mangaTable = $('#mangaTable').DataTable(getMangaTableOptions([...parsedData.englishMangaSites, ...parsedData.foreignMangaSites]))
+        const scansTable = $('#scansTable').DataTable(getMangaTableOptions([...parsedData.englishMangaScans, ...parsedData.foreignMangaScans]))
 
 	    // NOVEL SITES ------------------------------
 	    const lightNovelTable = $('#lightNovelTable').DataTable(getLightNovelTableOptions(parsedData.lightNovels))
 	    const visualNovelTable = $('#visualNovelTable').DataTable(getVisualNovelTableOptions(parsedData.visualNovels))
-	    // Handles using a single search bar for multiple tables
-	    $('#novelTableSearch').on('keyup click', function () {
-	      lightNovelTable.tables().search($(this).val()).draw()
-	      visualNovelTable.tables().search($(this).val()).draw()
-	    })
+
 	    // APPLICATIONS ------------------------------
 	    const iosApplicationsTable = $('#iosApplications').DataTable(getApplicationTableOptions(parsedData.iOSApplications))
 	    const androidApplicationsTable = $('#androidApplications').DataTable(getApplicationTableOptions(parsedData.androidApplications))
@@ -325,13 +310,22 @@ window.onload = e => {
 	    const mangaApplicationsTable = $('#mangaApplications').DataTable(getApplicationTableOptions(parsedData.mangaApplications))
 	    const macOSXApplicationsTable = $('#macApplications').DataTable(getApplicationTableOptions(parsedData.macOSApplications))
 	    const browserExtensionsTable = $('#browserExtensionsTable').DataTable(getApplicationTableOptions(parsedData.browserExtensions))
-	    // Handles using a single search bar for multiple tables
-	    $('#applicationsTableSearch').on('keyup click', function () {
-	      iosApplicationsTable.tables().search($(this).val()).draw()
-	      androidApplicationsTable.tables().search($(this).val()).draw()
-	      mangaApplicationsTable.tables().search($(this).val()).draw()
-	      macOSXApplicationsTable.tables().search($(this).val()).draw()
-	      browserExtensionsTable.tables().search($(this).val()).draw()
-	    })
+
+        // Handles using a single search bar for multiple tables
+        $('#tableSearch').on('keyup click', function () {
+          animeEnglishTable.tables().search($(this).val()).draw()
+          animeForeignTable.tables().search($(this).val()).draw()
+          animeDownloadTable.tables().search($(this).val()).draw()
+          mangaTable.tables().search($(this).val()).draw()
+          scansTable.tables().search($(this).val()).draw()
+          lightNovelTable.tables().search($(this).val()).draw()
+          visualNovelTable.tables().search($(this).val()).draw()
+          iosApplicationsTable.tables().search($(this).val()).draw()
+          androidApplicationsTable.tables().search($(this).val()).draw()
+          windowsApplicationsTable.tables().search($(this).val()).draw()
+          mangaApplicationsTable.tables().search($(this).val()).draw()
+          macOSXApplicationsTable.tables().search($(this).val()).draw()
+          browserExtensionsTable.tables().search($(this).val()).draw()
+        })
 	  })
 }
