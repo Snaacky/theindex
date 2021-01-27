f64b1712d1d7beca925d4bb94d2486633a5f675c
Yasamato <agent77326@gmail.com>
2021-01-27 11:30:29 +0100

    Fix umami

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,10 @@
 if (window.location.hostname === "piracy.moe") {
-    document.querySelector("body")
-        .innerHTML += '<script async defer data-website-id="6e4610bb-d364-413d-a371-2601b31a4c9b" ' +
-        'src="https://stats.piracy.moe/umami.js"></script>'
+    let script = document.createElement('script')
+    script.src = "https://stats.piracy.moe/umami.js"
+    script.async = true
+    script.defer = true
+    script.setAttribute("data-website-id", "6e4610bb-d364-413d-a371-2601b31a4c9b")
+    document.body.appendChild(script)
 } else {
     console.error("You are not viewing the index under the real domain piracy.moe, but", window.location.hostname)
 }
