2361e80ba1a729c8a75c65c2be025ce4a9cbb49f
Yasamato <agent77326@gmail.com>
2021-02-10 14:38:46 +0100

    Readd autobuilds and wsgi

--- app.py
+++ app.py
@@ -1,15 +1,61 @@
-from flask import Flask, send_file
+import os
+
+from flask import Flask, redirect, url_for, send_file, jsonify, render_template
+from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
+
+import config
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'  # ONLY FOR DEBUGGING!
+
+app = Flask(__name__, static_folder="static")
+
+app.secret_key = config.SECRET_KEY
+
+# Discord OAuth2 for editor
+app.config["DISCORD_CLIENT_ID"] = config.DISCORD_CLIENT_ID
+app.config["DISCORD_CLIENT_SECRET"] = config.DISCORD_CLIENT_SECRET
+app.config["DISCORD_REDIRECT_URI"] = config.DISCORD_REDIRECT_URI
+app.config["DISCORD_BOT_TOKEN"] = config.DISCORD_BOT_TOKEN
+discord = DiscordOAuth2Session(app)
 
 
-def create_app():
-    app = Flask(__name__, static_folder="static")
-
-    @app.route("/")
-    def index():
-        return send_file("static/index.html")
-
-    return app
+@app.route("/")
+def index():
+    return send_file(os.path.join("static", "index.html"))
 
 
+@app.route("/login/")
+def login():
+    return discord.create_session()
+
+
+@app.route("/logout/")
+def logout():
+    discord.revoke()
+    return redirect(url_for("index"))
+
+
+@app.route("/callback/")
+def callback():
+    discord.callback()
+    user = discord.fetch_user()
+    if user.id not in config.WHITELISTED_USERS:
+        return redirect(url_for(".logout"))
+    return redirect(url_for(".editor"))
+
+
+@app.errorhandler(Unauthorized)
+def redirect_unauthorized(e):
+    return redirect(url_for("login"))
+
+
+@app.route("/editor/")
+# @requires_authorization
+def editor():
+    user = discord.fetch_user()
+    return render_template("editor.html", user=user)
+
+
+# For testing purposes
 if __name__ == "__main__":
-    create_app().run(host='0.0.0.0', port=8080, debug=False)
+    app.run(port=8080, debug=True)
---
+++ config.example.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "random_key_for_flask"
+DISCORD_CLIENT_ID = "your_discord_client_id"
+DISCORD_CLIENT_SECRET = "your_discord_client_secret"
+DISCORD_REDIRECT_URI = "http://localhost:5000/callback"
+DISCORD_BOT_TOKEN = "your_discord_bot_token"
+FLASK_PORT = 5000
+FLASK_DEBUG = True
+WHITELISTED_USERS = [00000000000, 0000000000, 00000000]
