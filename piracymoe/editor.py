95be8c728e36423d3cfc7864c186537d2ece0fe9
Snaacky <Snaacky@pm.me>
2021-04-01 06:13:08 -0400

    fixed route

--- piracymoe/editor.py
+++ piracymoe/editor.py
@@ -14,7 +14,7 @@ def is_login():
     return jsonify({"edit": False})
 
 
-@bp.route("/user/logina/")
+@bp.route("/user/login/")
 def login():
     if app.discord.authorized:
         return redirect(url_for("editor.login"))
