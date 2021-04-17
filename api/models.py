be0646216979cbee22a3fc26c6c159b62481af80
Sanderstarr <55017867+Sanderstarr@users.noreply.github.com>
2021-04-17 23:42:56 +0200

    Lowered varchar limit since it was to high

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from app import db
 
 class Tab(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000), nullable=False)
-    description = db.Column(db.String(1000))
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
     tables = db.relationship(
         'Table',
         backref="tab",
@@ -23,8 +23,8 @@ class Tab(db.Model):
 
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000), nullable=False)
-    description = db.Column(db.String(1000))
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
     tab_id = db.Column(db.Integer, db.ForeignKey('tab.id'))
     order = db.Column(db.Integer)
     hidden = db.Column(db.Boolean, default=False)
@@ -54,7 +54,7 @@ class Table(db.Model):
 
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(1000), nullable=False)
+    data = db.Column(db.String(255), nullable=False)
     table_id = db.Column(db.Integer, db.ForeignKey('table.id'))
 
     def to_dict(self):
@@ -67,10 +67,10 @@ class Data(db.Model):
 
 class Column(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000), nullable=False)
-    column_type = db.Column(db.String(1000), default="text")
-    key = db.Column(db.String(1000))
-    description = db.Column(db.String(1000))
+    name = db.Column(db.String(255), nullable=False)
+    column_type = db.Column(db.String(255), default="text")
+    key = db.Column(db.String(255))
+    description = db.Column(db.String(255))
 
     def to_dict(self):
         return {
