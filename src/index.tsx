7eb8b15fc25307283698677343c2383d970eb1d9
Yasamato <agent77326@gmail.com>
2021-04-17 19:17:57 +0200

    Update src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
