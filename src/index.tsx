04aaa67964b3c32343c2390325fe0a81b2458ad3
Yasamato <agent77326@gmail.com>
2021-04-17 19:37:18 +0200

    Remove never used code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
@@ -10,8 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
