4a7451378e5eb968b250115fb425673390ce27b0
Sanderstarr <55017867+Sanderstarr@users.noreply.github.com>
2021-04-19 15:48:08 +0200

    script optimization

--- api/init.py
+++ api/init.py
@@ -145,18 +145,18 @@ def transfer_table(data, table):
 
 if __name__ == "__main__":
 
-    # checks if at least 1 tab exists in the database
     with create_app().app_context():
-        res = Table.query.get(1)
-        if res:
+
+        # Checks the database to see if there's at least 1 tab
+        if Table.query.get(1):
             exit()
 
-    with open(os.path.join('static', 'columns.json')) as json_file:
-        columns_data = json.load(json_file)
-    with open(os.path.join('static', 'tables.json')) as json_file:
-        tables_data = json.load(json_file)
+        # Loads the data that'll get imported
+        with open(os.path.join('static', 'columns.json')) as json_file:
+            columns_data = json.load(json_file)
+        with open(os.path.join('static', 'tables.json')) as json_file:
+            tables_data = json.load(json_file)
 
-    with create_app().app_context():
         for key in columns_data["keys"].keys():
             c = columns_data["keys"][key]
             col = Column(
