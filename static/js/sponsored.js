700d1201a25c2061c2f8c12bfff0f6ca2a180c86
Snaacky <snaacky@pm.me>
2021-07-23 04:42:50 -0400

    Added extra <br> to sponsored listing

--- static/js/sponsored.js
+++ static/js/sponsored.js
@@ -23,7 +23,7 @@ window.addEventListener("load", () => {
             ' <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">360p</span>' +
             ' x <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Subs</span> / ' +
             ' <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Dubs</span> ' +
-            ' + <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Downloads</span>'
+            ' + <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Downloads</span><br><br>'
         }
     ]
 
