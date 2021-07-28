b90cab31b38e33e927740058d741477f1d4ac6f5
Yasamato <agent77326@gmail.com>
2021-07-28 18:22:49 +0200

    Add next auth session component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 // custom css
 import '../styles/global.css'
 import '../styles/arrayInput.css'
+import {Provider} from "next-auth/client"
 
 export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+    </Provider>
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,27 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        //
+        async signIn(user, account, profile) {
+            console.log("Sign In event:", user, account, profile)
+            return true
+        },
+
+        // we want to access the user id
+        async session(session, user) {
+            if (user) {
+                session.user.uid = user.id
+            }
+            return session
+        },
+        async jwt(token, user) {
+            if (user) {
+                token.uid = user.id
+            }
+            return token
+        }
+    },
 
     // A database is optional, but required to persist accounts in a database
     database: process.env.DATABASE_URL + "/index?entityPrefix=nextauth_",
