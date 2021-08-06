46ef925d1844df9a676c3cbe9d1d5576c47ef236
Yasamato <agent77326@gmail.com>
2021-08-06 17:20:41 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -3,6 +3,7 @@ import {signIn} from "next-auth/client"
 import {Icon} from "react-icons-kit"
 import {signIn as iconSignIn} from "react-icons-kit/fa/signIn"
 import Head from "next/head"
+import Image from "next/image"
 
 export default function Login() {
     return <div className={styles.wrapper}>
@@ -10,12 +11,15 @@ export default function Login() {
             <title>
                 Access denied
             </title>
-            <meta name="robots" content="noindex" />
+            <meta name="robots" content="noindex"/>
         </Head>
-        <h3 className={"mb-3"}>
+        <h3 className={"mt-5 mb-3"}>
             This page is protected
         </h3>
-        <button className={"btn btn-outline-success"} onClick={signIn}>
+
+        <Image width={128} height={128} src={"/img/mioWAAH.gif"} alt={""}
+               className={"rounded"}/>
+        <button className={"btn btn-outline-success mt-5"} onClick={signIn}>
             <Icon icon={iconSignIn}/> Sign In
         </button>
     </div>
