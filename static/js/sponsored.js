c32032f262453f19ad1c2f0773b1d444234f5f7b
Snaacky <snaacky@pm.me>
2021-08-18 19:12:02 -0400

    Added sponsored section to manga tab, added TodayManga sponsored listing

--- static/js/sponsored.js
+++ static/js/sponsored.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    const sponsoredList = [
+    const sponsoredListAnime = [
         {
-            id: "sponsored1-3",
+            id: "sponsored-anime-1-3",
             title: "KimAnime.com",
             url: "https://kimanime.com",
             description: 'Watch in ' +
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
             ' + <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Downloads</span>'
         },
         {
-            id: "sponsored4",
+            id: "sponsored-anime-4",
             title: "11anime",
             url: "https://1anime.to/",
             description: 'Watch in ' +
@@ -23,11 +23,19 @@ window.addEventListener("load", () => {
             ' offering both <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Subs</span>' +
             ' and <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">Dubs</span>, with a clean UI'
         },
-
+    ]
+    
+    const sponsoredListManga = [
+        {
+            id: "sponsored-manga-1",
+            title: "TodayManga",
+            url: "https://todaymanga.com",
+            description: ' A chapter a day keeps the doctors away! It doesn\'t make any sense, but we got your attention!'
+        }
     ]
 
-    if (sponsoredList && sponsoredList.length > 0) {
-        sponsoredList.forEach(sponsored => {
+    if (sponsoredListAnime && sponsoredListAnime.length > 0) {
+        sponsoredListAnime.forEach(sponsored => {
             document.querySelector('#sponsoredAnime').innerHTML +=
                 '<div class="col" style="padding-right:1em;">' +
                 '<div class="card mx-auto pr-1" style="min-width: 160px; max-width: 19.4rem;"><div class="card-body bg-darker">' +
@@ -70,7 +78,54 @@ window.addEventListener("load", () => {
                 })
         })
     } else {
-        document.querySelector('#sponsoredAnime').remove()
+        document.querySelector('#sponsoredListAnime').remove()
+    }
+
+    if (sponsoredListManga && sponsoredListManga.length > 0) {
+        sponsoredListManga.forEach(sponsored => {
+            document.querySelector('#sponsoredManga').innerHTML +=
+                '<div class="col" style="padding-right:1em;">' +
+                '<div class="card mx-auto pr-1" style="min-width: 160px; max-width: 19.4rem;"><div class="card-body bg-darker">' +
+                '<h5 class="card-title">' +
+                '<div class="spinner-grow d-inline-block rounded-circle bg-secondary spinner-grow-sm" ' +
+                'id="online-' + sponsored["id"] + '" data-bs-toggle="tooltip" role="status"></div> ' +
+                sponsored["title"] + '</h5>' +
+                '<h6 class="card-subtitle mb-2 text-warning">' +
+                '<small><i class="bi bi-star-fill"></i></small>' +
+                '</h6>' +
+                '<p class="card-text d-none d-lg-block">' + sponsored["description"] + '</p>' +
+                '<a class="umami--click--sponsored--' + sponsored["title"].toLowerCase() + '" href="' + sponsored["url"] + '" target="_blank">' +
+                '<i class="bi bi-box-arrow-up-right"></i> Visit site' +
+                '</a>' +
+                '</div></div></div>'
+            checkOnlineStatus(sponsored['url'])
+                .then(status => {
+                    let onlineStatus = document.querySelector('#online-' + sponsored["id"])
+                    console.log("Sponsor online-check", onlineStatus, status)
+                    onlineStatus.classList.remove("spinner-grow")
+                    // remove previous color-state
+                    if (onlineStatus.classList.contains("bg-secondary")) {
+                        onlineStatus.classList.remove("bg-secondary")
+                    }
+
+                    // apply result color
+                    if (status["status"] === "unknown") {
+                        onlineStatus.classList.add("bg-warning")
+                        onlineStatus.setAttribute("title", "Unknown")
+                    } else if (status["status"] === "up") {
+                        onlineStatus.classList.add("label-yes")
+                        onlineStatus.setAttribute("title", "Online")
+                    } else {
+                        onlineStatus.classList.add("label-no")
+                        onlineStatus.setAttribute("title", "Offline")
+                    }
+
+                    // initialize Tooltip
+                    new bootstrap.Tooltip(onlineStatus)
+                })
+        })
+    } else {
+        document.querySelector('#sponsoredListManga').remove()
     }
 
 })
