223c102f8ce90c501d922ac3ed664be6598a561b
Snaacky <snaacky@pm.me>
2021-08-19 01:05:29 -0400

    More URL fixes

--- static/js/sponsored.js
+++ static/js/sponsored.js
@@ -16,7 +16,7 @@ window.addEventListener("load", () => {
         {
             id: "sponsored-anime-4",
             title: "11anime",
-            url: "https://11anime.to/",
+            url: "https://11anime.fr/",
             description: 'Watch in ' +
             ' <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">1080p</span>' +
             ' with <span class="badge rounded-pill" style="background-color: #ffc106;color: #202020;">No Ads</span>, ' +
