289af1cbe24625f4d875e11cc3b1ae8eaf8526a9
Yasamato <agent77326@gmail.com>
2021-08-26 12:21:29 +0200

    Limit full export to admin only

--- pages/api/export.js
+++ pages/api/export.js
@@ -1,6 +1,15 @@
+import {getSession} from "next-auth/client"
 import {exportData} from "../../lib/db/db"
+import {isAdmin} from "../../lib/session"
 
 export default async function handler(req, res) {
-    const d = await exportData()
-    res.status(200).json(d)
+    const session = await getSession({req})
+    if (isAdmin(session)) {
+        const d = await exportData()
+        res.status(200).json(d)
+    } else {
+        // Not Signed in
+        res.status(401).send("Not logged in or edits are not permitted")
+    }
+    res.end()
 }
