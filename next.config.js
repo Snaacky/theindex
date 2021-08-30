5eb85f3a2fde90ffe35e51b32fe8590b36cf8631
Yasamato <agent77326@gmail.com>
2021-08-30 10:26:18 +0200

    Add security headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
  * @type {import("next").NextConfig}
  */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; img-src *; script-src 'self' https://cdn.jsdelivr.net"
+                    }
+                ]
+            }
+        ]
+    },
     images: {
         domains: ["cdn.discordapp.com"]
     }
