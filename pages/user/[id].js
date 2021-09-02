460379913073c0b7975ce58e5f88ebbc048dbb2a
Yasamato <agent77326@gmail.com>
2021-09-02 09:26:27 +0200

    Add static pregeneration of users and added dicebear

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,53 +1,66 @@
 import {siteName} from "../../components/layout/Layout"
 import Head from "next/head"
 import Link from "next/link"
+import Image from "next/image"
 import {useSession} from "next-auth/client"
-import Loader from "../../components/loading"
-import {isAdmin} from "../../lib/session"
-import IconEdit from "../../components/icons/IconEdit"
+import {isAdmin, isCurrentUser} from "../../lib/session"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import DataBadge from "../../components/data/DataBadge"
 import {getUser, getUsers} from "../../lib/db/users"
 import useSWR from "swr"
 import Error from "../_error"
 
-export default function User({uid}) {
+export default function User({uid, user: staticUser}) {
     const [session] = useSession()
     const {data: user, error} = useSWR("/api/user/" + uid)
 
     if (error) {
         return <Error error={error} statusCode={error.status}/>
-    } else if (!user) {
-        return <Loader/>
     }
 
+    const name = user ? user.name : staticUser.name
+    const description = user ? user.description : staticUser.description
+    const image = user ? user.image : staticUser.image
+    const accountType = user ? user.accountType : staticUser.accountType
     return <>
         <Head>
             <title>
-                {"User " + user.name + " | " + siteName}
+                {"User " + name + " | " + siteName}
             </title>
             <meta name="robots" content="noindex, archive, follow"/>
-            <meta name="description" content={user.description}/>
+            <meta name="description" content={description}/>
         </Head>
 
         <div className={"card bg-2"}>
-            <div className="card-body">
-                <div className={"card-title"}>
-                    <h3>
-                        {user.name}
-                        <span className={"ms-2"} style={{fontSize: "1.2rem"}}>
-                            <DataBadge name={user.accountType} style={"primary"}/>
+            <div className="card-body pb-0">
+                <div className={"card-title row"}>
+                    <div className={"col-auto"} style={{
+                        height: "40px",
+                        overflow: "show"
+                    }}>
+                        <Image className={"rounded"} src={image} alt={"Profile picture of " + name} width={64}
+                               height={64}/>
+                    </div>
+                    <div className={"col"}>
+                        <h3>
+                            {name}
+                            <span className={"ms-2"} style={{fontSize: "1.2rem"}}>
+                            <DataBadge name={accountType} style={"primary"}/>
                             <div className={"float-end"}>
-                                {isAdmin(session) ? <Link href={"/edit/user/" + user.uid}>
+                                {isAdmin(session) || isCurrentUser(session, uid) ? <Link href={"/edit/user/" + uid}>
                                     <a title={"Edit user"} className={"ms-2"}>
-                                        <IconEdit/>
+                                        <FontAwesomeIcon icon={["fas", "cog"]}/>
                                     </a>
                                 </Link> : <></>}
                             </div>
                         </span>
-                    </h3>
+                        </h3>
+                    </div>
                 </div>
+            </div>
+            <div className={"card-body bg-6"}>
                 <p className={"card-text"}>
-                    {user.description || <span className={"text-muted"}>
+                    {description || <span className={"text-muted"}>
                         It seems quite empty here
                     </span>}
                 </p>
@@ -82,7 +95,8 @@ export async function getStaticProps({params}) {
     }
     return {
         props: {
-            uid: params.id
+            uid: params.id,
+            user
         },
         revalidate: 10
     }
