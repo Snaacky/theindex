38370ba76b243e170732ddef1d0ba9313961e90d
Yasamato <agent77326@gmail.com>
2021-09-03 12:55:50 +0200

    Fix permission issue for update list

--- pages/api/edit/list.js
+++ pages/api/edit/list.js
@@ -1,23 +1,26 @@
 import {getSession} from "next-auth/client"
 import {isAdmin, isCurrentUser, isLogin} from "../../../lib/session"
-import {addList, updateList} from "../../../lib/db/lists"
+import {addList, getList, updateList} from "../../../lib/db/lists"
 
 export default async function apiEditList(req, res) {
     const session = await getSession({req})
     const d = req.body
     if (isLogin(session)) {
-        if (d.owner !== "" && d.name !== "") {
-            if (typeof d._id === "undefined" && (isCurrentUser(session, d.owner) || isAdmin(session))) {
+        if (typeof d._id === "undefined" && (isCurrentUser(session, d.owner) || isAdmin(session))) {
+            if (d.owner !== "" && d.name !== "") {
                 await addList(d.owner, d.name, d.nsfw, d.description, d.columns)
                 res.status(200).send("Ok")
-            } else if (isCurrentUser(session, d.owner) || isAdmin(session)) {
+            } else {
+                res.status(400).send("Missing owner or name")
+            }
+        } else {
+            const list = await getList(d._id)
+            if (isCurrentUser(session, list.owner) || isAdmin(session)) {
                 await updateList(d._id, d)
                 res.status(200).send("Ok")
             } else {
                 res.status(401).send("Not logged in or edits are not permitted")
             }
-        } else {
-            res.status(400).send("Missing owner or name")
         }
     } else {
         // Not Signed in
