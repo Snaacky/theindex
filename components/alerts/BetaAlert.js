ac34c71e4d795e3e68f94a2d224101ef8cc3985c
Yasamato <agent77326@gmail.com>
2021-09-13 20:37:05 +0200

    Fix BetaAlert link not being visible

--- components/alerts/BetaAlert.js
+++ components/alerts/BetaAlert.js
@@ -5,7 +5,7 @@ export default function BetaAlert() {
     if (show) {
         return <div className={"alert alert-dark alert-dismissible show"} role={"alert"}>
             You are viewing a beta-site. Some features may not be fully functional and contain bugs.
-            <a href={"https://piracy.moe"}>Click here</a> to go back to the old index.
+            <a href={"https://piracy.moe"} className={"alert-link"}>Click here</a> to go back to the old index.
             <button type="button" className="btn-close" aria-label="Close" onClick={() => setShow(false)}/>
         </div>
     }
