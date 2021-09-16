6e79a96c5a03dd8384a7bbc5efb7ebc360d536dd
Yasamato <agent77326@gmail.com>
2021-09-16 20:44:12 +0200

    Add meta tags to NotAdmin and NotLogin

--- components/layout/NotAdmin.js
+++ components/layout/NotAdmin.js
@@ -4,14 +4,27 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import Head from "next/head"
 import Image from "next/image"
 
+const title = "Access denied - " + process.env.NEXT_PUBLIC_SITE_NAME
+const description = "You are missing privileges to view the site"
+
 export default function NotAdmin() {
     return <div className={styles.wrapper}>
         <Head>
             <title>
                 Access denied
             </title>
-            <meta name="robots" content="noindex"/>
-            <meta name={"description"} content={"You are missing privileges to view the site"}/>
+
+            <meta name="robots" content="noindex, noarchive"/>
+
+            <meta property="og:title" content={title}/>
+            <meta name="twitter:title" content={title}/>
+
+            <meta name="description" content={description}/>
+            <meta property="og:description" content={description}/>
+            <meta name="twitter:description" content={description}/>
+
+            <meta name="twitter:image" content={process.env.NEXT_PUBLIC_DOMAIN + "/icons/logo.png"}/>
+            <meta property="og:image" content={process.env.NEXT_PUBLIC_DOMAIN + "/icons/logo.png"}/>
         </Head>
         <h3 className={"mt-5 mb-3"}>
             This page is protected
--- components/layout/NotLogin.js
+++ components/layout/NotLogin.js
@@ -4,14 +4,26 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import Head from "next/head"
 import Image from "next/image"
 
+const title = "Login to continue - " + process.env.NEXT_PUBLIC_SITE_NAME
+const description = "You need to login to view the site"
+
 export default function NotLogin() {
     return <div className={styles.wrapper}>
         <Head>
             <title>
-                Access denied
+                Login to continue
             </title>
-            <meta name="robots" content="noindex"/>
-            <meta name={"description"} content={"You need to login to view the site"}/>
+            <meta name="robots" content="noindex, noarchive"/>
+
+            <meta property="og:title" content={title}/>
+            <meta name="twitter:title" content={title}/>
+
+            <meta name="description" content={description}/>
+            <meta property="og:description" content={description}/>
+            <meta name="twitter:description" content={description}/>
+
+            <meta name="twitter:image" content={process.env.NEXT_PUBLIC_DOMAIN + "/icons/logo.png"}/>
+            <meta property="og:image" content={process.env.NEXT_PUBLIC_DOMAIN + "/icons/logo.png"}/>
         </Head>
         <h3 className={"mt-5 mb-3"}>
             This page is protected
