29c5e4a5ebc0778795c5a9784d4c2f89fe349a33
Yasamato <agent77326@gmail.com>
2021-09-16 22:06:59 +0200

    Fix user creation and pfp changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
-import {addUser, getUser, userExists} from "../../../lib/db/users"
-import {findOne} from "../../../lib/db/db"
+import {addUser, getUser} from "../../../lib/db/users"
 
 const discord = Providers.Discord({
     clientId: process.env.DISCORD_CLIENT_ID,
@@ -15,36 +14,33 @@ const nextAuth = NextAuth({
         // we want to access the user id
         async session(session, user) {
             if (user) {
-                session.user.uid = user.id
-                if (!(await userExists(user.id))) {
-                    const accountData = await findOne("nextauth_accounts", {userId: user.id})
-                    const accountType = (
-                        typeof process.env.SETUP_WHITELIST_DISCORD_ID !== "undefined" &&
-                        process.env.SETUP_WHITELIST_DISCORD_ID !== "" &&
-                        accountData.providerAccountId === process.env.SETUP_WHITELIST_DISCORD_ID.toString() ?
-                            "admin" : "user"
-                    )
-                    await addUser({
-                        uid: user.id,
-                        accountType
-                    })
-                }
-                const uData = await getUser(user.id)
-                session.user.accountType = uData.accountType
-                session.user.description = uData.description
+                const {uid, accountType} = await getUser(user.id)
+                session.user.uid = uid
+                session.user.accountType = accountType
             }
+            console.log(session)
             return session
         },
-        async jwt(token, user) {
-            if (user) {
-                token.uid = user.id
-            }
-            return token
+    },
+    events: {
+        async createUser({user, account}) {
+            console.log("Create new user", user.name)
+            const accountType = (
+                typeof process.env.SETUP_WHITELIST_DISCORD_ID !== "undefined" &&
+                process.env.SETUP_WHITELIST_DISCORD_ID !== "" &&
+                account.id === process.env.SETUP_WHITELIST_DISCORD_ID.toString() ?
+                    "admin" : "user"
+            )
+            await addUser({
+                uid: user.id.toString(),
+                accountType
+            })
         }
     },
 
     // A database is optional, but required to persist accounts in a database
     database: process.env.DATABASE_URL + "/index?entityPrefix=nextauth_",
+    secret: process.env.NEXTAUTH_SECRET
 })
 
 export default nextAuth
--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
         if (isLogin(session)) {
             result = await getUserWithLists(session.user.uid)
         } else {
-            return res.status(401).json({})
+            return res.status(200).json({})
         }
     } else {
         result = await getUserWithLists(req.query.id)
