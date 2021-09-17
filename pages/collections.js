894f9391af4e227cc5d1f0b7878ddc51bc41677f
Yasamato <agent77326@gmail.com>
2021-09-17 11:34:00 +0200

    Add hotfix to cleanup code to cleanup db residues of not existing items in collections

--- pages/collections.js
+++ pages/collections.js
@@ -5,7 +5,8 @@ import CollectionBoard from "../components/boards/CollectionBoard"
 import useSWR from "swr"
 import {useSession} from "next-auth/client"
 import {isEditor} from "../lib/session"
-import {getCollections} from "../lib/db/collections"
+import {getCollections, updateCollection} from "../lib/db/collections"
+import {getItem} from "../lib/db/items";
 
 const title = "Collections on " + process.env.NEXT_PUBLIC_SITE_NAME
 const description = "Collections are like a curated table listing various items. Every entry is done by hand from the editor-team"
@@ -46,6 +47,20 @@ export default function Collections({collections: staticCollections}) {
 
 export async function getStaticProps() {
     const collections = await getCollections()
+
+    // TODO: remove cleanup code, should be handled by db
+    collections.forEach(collection => {
+        collection.items.forEach(itemId => {
+            getItem(itemId).then(item => {
+                if (typeof item === "undefined" || item === null) {
+                    updateCollection(collection._id, {
+                        items: collection.items.filter(i => i !== itemId)
+                    })
+                }
+            })
+        })
+    })
+
     return {
         props: {
             collections
