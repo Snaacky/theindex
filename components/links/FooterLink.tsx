9f32f90798937e9d5deda081675ae4c6c6ee8ecb
version13 <crap.developer@protonmail.com>
2021-09-20 22:22:18 +0300

    Add FooterLink component and fix .prettierrc styling

---
+++ components/links/FooterLink.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconName } from '@fortawesome/fontawesome-common-types'
+
+// import styles
+import styles from './FooterLink.module.css'
+
+type Props = {
+  name: string
+  url: string
+  icon?: IconName
+  customIcon?: boolean
+  customIconName?: string
+}
+
+const FooterLink: FC<Props> = ({
+  name,
+  url,
+  icon,
+  customIcon,
+  customIconName,
+}) => {
+  return (
+    <div className='d-flex icon-link-hover my-2'>
+      <a
+        href={url}
+        target='_blank'
+        rel='noreferrer'
+        aria-label={name}
+        className='d-flex align-items-center'
+      >
+        {customIcon && (
+          <Image
+            src={`/icons/${customIconName}.png`}
+            height={18}
+            width={18}
+            alt={name}
+          />
+        )}
+        <FontAwesomeIcon icon={['fab', `${icon}`]} fixedWidth={true} />
+        <span className='ms-2'>{name}</span>
+      </a>
+    </div>
+  )
+}
+
+export default FooterLink
