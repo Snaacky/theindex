2ab6b224a72f3c73fa9f323fc1c4d59f51b831e5
Yasamato <agent77326@gmail.com>
2021-09-28 14:38:12 +0200

    Correct other merges gone wrong
    I probably messed up head and origin

--- components/cards/ItemCard.js
+++ components/cards/ItemCard.js
@@ -13,41 +13,58 @@ export default function ItemCard({
 }) {
   const column = splitColumnsIntoTypes(columns, item)
 
-export default function ItemCard(
-    {
-        item,
-        columns = [],
-        add = null,
-        remove = null,
-        move = null
-    }) {
-
-    const column = splitColumnsIntoTypes(columns, item)
-
-    return <Card type={"item"} content={item} add={add} remove={remove} move={move} bodyContent={<>
-        {column.yes.length > 0 ?
-            <div className={"d-flex flex-wrap mb-1"}>
-                {column.yes.map(c => <BoolValue data={true} column={c} sponsor={item.sponsor} key={c._id}/>)}
-            </div> : <></>
-        }
-        {!item.sponsor && column.no.length > 0 ?
-            <div className={"d-flex flex-wrap mb-1"}>
-                {column.no.map(c => <BoolValue data={false} column={c} key={c._id}/>)}
-            </div> : <></>
-        }
-        {column.array.length > 0 ?
-            <div className={"d-flex flex-wrap mb-1"}>
-                {column.array.map(c => {
-                    return <div key={c._id}>
-                        <Link href={"/column/" + c.urlId}>
-                            <a className={"me-2"} title={"View column " + c.name}>
-                                {c.name}:
-                            </a>
-                        </Link>
-                        <ArrayValue data={item.data[c._id]} column={c}/>
-                    </div>
-                })}
-            </div> : <></>
-        }
-    </>}/>
+  return (
+    <Card
+      type={'item'}
+      content={item}
+      add={add}
+      remove={remove}
+      move={move}
+      bodyContent={
+        <>
+          {column.yes.length > 0 ? (
+            <div className={'d-flex flex-wrap mb-1'}>
+              {column.yes.map((c) => (
+                <BoolValue
+                  data={true}
+                  column={c}
+                  sponsor={item.sponsor}
+                  key={c._id}
+                />
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+          {!item.sponsor && column.no.length > 0 ? (
+            <div className={'d-flex flex-wrap mb-1'}>
+              {column.no.map((c) => (
+                <BoolValue data={false} column={c} key={c._id} />
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+          {column.array.length > 0 ? (
+            <div className={'d-flex flex-wrap mb-1'}>
+              {column.array.map((c) => {
+                return (
+                  <div key={c._id}>
+                    <Link href={'/column/' + c.urlId}>
+                      <a className={'me-2'} title={'View column ' + c.name}>
+                        {c.name}:
+                      </a>
+                    </Link>
+                    <ArrayValue data={item.data[c._id]} column={c} />
+                  </div>
+                )
+              })}
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      }
+    />
+  )
 }
--- components/data/DataBadge.js
+++ components/data/DataBadge.js
@@ -1,9 +1,17 @@
 import styles from './DataBadge.module.css'
 
-export default function DataBadge({data, name, style = "", sponsor = false}) {
-    const bgStyle = style !== "" ? style : (data === true ? "success" : (data === false ? "danger" : "secondary"))
-    return <div className={styles.badge + " badge rounded-pill bg-" + bgStyle + " " + (sponsor ? styles.sponsor + " text-dark" : "")}>
-        {name}
+export default function DataBadge({ data, name, style = '' }) {
+  const bgStyle =
+    style !== ''
+      ? style
+      : data === true
+      ? 'success'
+      : data === false
+      ? 'danger'
+      : 'secondary'
+  return (
+    <div className={styles.badge + ' badge rounded-pill bg-' + bgStyle}>
+      {name}
     </div>
   )
 }
