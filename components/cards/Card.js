43fa9416b4fbd1c5e2a21600184e3ed5303372d3
Yasamato <agent77326@gmail.com>
2021-09-28 14:35:36 +0200

    Fix card merge gone wrong

--- components/cards/Card.js
+++ components/cards/Card.js
@@ -32,18 +32,60 @@ export default function Card({
     return <Loader />
   }
 
-    if (typeof content === "undefined") {
-        return <Loader/>
-    }
+  if (typeof content === 'undefined') {
+    return <Loader />
+  }
 
-    return <div className={styles.card + " " + (content.sponsor ? styles.sponsored : "") + " card bg-2 mb-2 me-2"}>
-        <div className="row g-0">
-            {move !== null ? <div className={styles.sorter + " col-auto"}>
-                <a onClick={() => move(-1)}>
-                    <FontAwesomeIcon icon={["fas", "chevron-up"]}/>
-                </a>
-                <a onClick={() => move(1)}>
-                    <FontAwesomeIcon icon={["fas", "chevron-down"]}/>
+  return (
+    <div
+      className={
+        styles.card +
+        ' ' +
+        (content.sponsor ? styles.sponsored : '') +
+        ' card bg-2 mb-2 me-2'
+      }
+    >
+      <div className='row g-0'>
+        {move !== null ? (
+          <div className={styles.sorter + ' col-auto'}>
+            <a onClick={() => move(-1)}>
+              <FontAwesomeIcon icon={['fas', 'chevron-up']} />
+            </a>
+            <a onClick={() => move(1)}>
+              <FontAwesomeIcon icon={['fas', 'chevron-down']} />
+            </a>
+          </div>
+        ) : (
+          <></>
+        )}
+        {imageUrl !== '' ? (
+          <div className={'col-auto'}>
+            <Link href={hrefString}>
+              <a title={'View ' + type + ' ' + content.name}>
+                <Image
+                  src={imageUrl}
+                  className='img-fluid rounded-start'
+                  alt='...'
+                  width={128}
+                  height={128}
+                />
+              </a>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className='col'>
+          <div className={'card-body d-flex flex-column p-2 h-100'}>
+            <h5 className={styles.title + ' card-title'}>
+              {typeof content.urls !== 'undefined' ? (
+                <OnlineStatus url={content.urls[0] ?? ''} />
+              ) : (
+                <></>
+              )}
+              <Link href={hrefString}>
+                <a title={'View ' + type + ' ' + content.name}>
+                  {content.name}
                 </a>
               </Link>
               {typeof content.urls !== 'undefined' ? (
@@ -57,69 +99,40 @@ export default function Card({
                     <a title={'Edit ' + type} className={'ms-2'}>
                       <IconEdit />
                     </a>
-                </Link>
-            </div> : <></>}
-            <div className="col">
-                <div className={"card-body d-flex flex-column p-2 h-100"}>
-                    <h5 className={styles.title + " card-title"}>
-                        {typeof content.urls !== "undefined" ?
-                            <OnlineStatus url={content.urls[0] ?? ''}/> : <></>}
-                        <Link href={hrefString}>
-                            <a title={"View " + type + " " + content.name}>
-                                {content.name}
-                            </a>
-                        </Link>
-                        {typeof content.urls !== "undefined" ? <IconNewTabLink url={content.urls[0]}/> : <></>}
-                        {canEdit(session, type) ? <>
-                            <Link href={"/edit/" + type + "/" + content._id}>
-                                <a title={"Edit " + type} className={"ms-2"}>
-                                    <IconEdit/>
-                                </a>
-                            </Link>
-                        </> : ""}
-                        {content.sponsor ?
-                            <span className={styles.sponsorBadge + " ms-2 float-end"}>
-                                <span className={styles.sponsorIcon}>
-                                    <IconSponsor size="xs"/>
-                                </span>
-                                Sponsored
-                            </span> : <></>
-                        }
-                        <span className={styles.action}>
-                            {content.nsfw ?
-                                <span className={"ms-2"}>
-                                    <IconNSFW/>
-                                </span> : <></>
-                            }
-                            {content.accountType ?
-                                <span className={"ms-2"}>
-                                    <DataBadge name={content.accountType} style={"primary"}/>
-                                </span> : <></>
-                            }
-                            {type === "item" ?
-                                <span className={"float-end"}>
-                                    <span className={"ms-2"}>
-                                        <IconStar item={content}/>
-                                    </span>
-                                    <span className={"ms-2"}>
-                                        <IconBookmark item={content}/>
-                                    </span>
-                                </span> : <></>
-                            }
-                            {add !== null ?
-                                <a title={"Add " + type} className={styles.link + " float-end"} onClick={add}>
-                                    <IconAdd/>
-                                </a> : <></>}
-                            {remove !== null ?
-                                <a title={"Delete " + type} className={styles.link + " float-end"}
-                                   onClick={remove}>
-                                    <IconDelete/>
-                                </a> : <></>}
-                        </span>
-                    </h5>
-
-                    <span className={styles.description + " card-text"}>
-                        {content.description}
+                  </Link>
+                </>
+              ) : (
+                ''
+              )}
+              {content.sponsor ? (
+                <span className={styles.sponsorBadge + ' ms-2 float-end'}>
+                  <span className={styles.sponsorIcon}>
+                    <IconSponsor size='xs' />
+                  </span>
+                  Sponsored
+                </span>
+              ) : (
+                <></>
+              )}
+              <span className={styles.action}>
+                {content.nsfw ? (
+                  <span className={'ms-2'}>
+                    <IconNSFW />
+                  </span>
+                ) : (
+                  <></>
+                )}
+                {content.accountType ? (
+                  <span className={'ms-2'}>
+                    <DataBadge name={content.accountType} style={'primary'} />
+                  </span>
+                ) : (
+                  <></>
+                )}
+                {type === 'item' ? (
+                  <span className={'float-end'}>
+                    <span className={'ms-2'}>
+                      <IconStar item={content} />
                     </span>
                     <span className={'ms-2'}>
                       <IconBookmark item={content} />
