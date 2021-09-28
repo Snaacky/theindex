c6f69cc693247fbfe2cbce499163c5d8e79b7c70
Yasamato <agent77326@gmail.com>
2021-09-28 14:27:59 +0200

    npx prettier --write .

--- components/data/BoolValue.js
+++ components/data/BoolValue.js
@@ -1,14 +1,21 @@
 import Link from 'next/link'
 import DataBadge from './DataBadge'
 
-export default function BoolValue({data, column, sponsor = false, onChange = null}) {
-    if (onChange === null) {
-        return <Link href={"/column/" + column.urlId}>
-            <a className={"me-2"} title={"View column " + column.name}>
-                <DataBadge data={data} name={column.name} sponsor={sponsor}/>
-            </a>
-        </Link>
-    }
+export default function BoolValue({
+  data,
+  column,
+  sponsor = false,
+  onChange = null,
+}) {
+  if (onChange === null) {
+    return (
+      <Link href={'/column/' + column.urlId}>
+        <a className={'me-2'} title={'View column ' + column.name}>
+          <DataBadge data={data} name={column.name} sponsor={sponsor} />
+        </a>
+      </Link>
+    )
+  }
 
   return (
     <a
