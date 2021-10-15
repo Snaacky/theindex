9183cd6812c54e8eee7cd836487be6cc4a744b4c
version13 <crap.developer@protonmail.com>
2021-10-16 00:39:12 +0300

    add typescript beta alert with localstorage

---
+++ components/alerts/BetaAlert.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+
+const nextJSLocation = typeof window === 'undefined'
+
+const BetaAlert = () => {
+  const [show, setShow] = useState<boolean>()
+
+  const handleShow = () => {
+    const obj = {
+      value: true,
+    }
+
+    if (!nextJSLocation) {
+      localStorage.setItem('alert', JSON.stringify(obj))
+      setShow(false)
+    }
+  }
+
+  // just forcing a refresh if we're
+  useEffect(() => {
+    const getAlertState = !nextJSLocation && localStorage.getItem('alert')
+    const parseLocalStorage = getAlertState && JSON.parse(getAlertState)
+    if (!parseLocalStorage?.value) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }, [!nextJSLocation && localStorage])
+
+  return (
+    <>
+      {show && (
+        <div
+          className={'alert alert-dark alert-dismissible show'}
+          role={'alert'}
+        >
+          You are viewing a beta-site. Some features may not be fully functional
+          and contain bugs.{' '}
+          <a href={'https://piracy.moe'} className={'alert-link'}>
+            Click here
+          </a>
+          to go back to the old index.
+          <button
+            type='button'
+            className='btn-close'
+            aria-label='Close'
+            onClick={handleShow}
+          />
+        </div>
+      )}
+
+      {!show && <div></div>}
+    </>
+  )
+}
+
+export default BetaAlert
