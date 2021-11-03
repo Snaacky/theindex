ef20c389ece6775e271437de533859b111aaf5ab
Yasamato <agent77326@gmail.com>
2021-11-03 13:47:01 +0100

    Fix linting

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,14 @@ import ListCard from '../components/cards/ListCard'
 import LibraryCard from '../components/cards/LibraryCard'
 
 const description =
-    'The best places to stream your favorite anime shows online or download them for free and watch in sub or dub. Supports manga, light novels, hentai, and apps.'
+  'The best places to stream your favorite anime shows online or download them for free and watch in sub or dub. Supports manga, light novels, hentai, and apps.'
 
 export default function Home({
-                               trendingLibraries: staticTrendingLibraries,
-                               trendingItem: staticTrendingItem,
-                               trendingCollection: staticTrendingCollection,
-                               trendingList: staticTrendingList,
-                             }) {
+  trendingLibraries: staticTrendingLibraries,
+  trendingItem: staticTrendingItem,
+  trendingCollection: staticTrendingCollection,
+  trendingList: staticTrendingList,
+}) {
   let { data: libraries } = useSWR('/api/popular/libraries')
   let { data: items } = useSWR('/api/popular/items')
   let { data: collections } = useSWR('/api/popular/collections')
@@ -27,130 +27,130 @@ export default function Home({
   lists = (lists || staticTrendingList).slice(0, 10)
 
   return (
-      <>
-        <Head>
-          <title>{process.env.NEXT_PUBLIC_SITE_NAME}</title>
+    <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_SITE_NAME}</title>
 
-          <meta property='og:title' content={process.env.NEXT_PUBLIC_SITE_NAME} />
-          <meta
-              name='twitter:title'
-              content={process.env.NEXT_PUBLIC_SITE_NAME}
-          />
+        <meta property='og:title' content={process.env.NEXT_PUBLIC_SITE_NAME} />
+        <meta
+          name='twitter:title'
+          content={process.env.NEXT_PUBLIC_SITE_NAME}
+        />
 
-          <meta name='description' content={description} />
-          <meta property='og:description' content={description} />
-          <meta name='twitter:description' content={description} />
+        <meta name='description' content={description} />
+        <meta property='og:description' content={description} />
+        <meta name='twitter:description' content={description} />
 
-          <meta
-              name='twitter:image'
-              content={process.env.NEXT_PUBLIC_DOMAIN + '/icons/logo.png'}
-          />
-          <meta
-              property='og:image'
-              content={process.env.NEXT_PUBLIC_DOMAIN + '/icons/logo.png'}
-          />
-        </Head>
+        <meta
+          name='twitter:image'
+          content={process.env.NEXT_PUBLIC_DOMAIN + '/icons/logo.png'}
+        />
+        <meta
+          property='og:image'
+          content={process.env.NEXT_PUBLIC_DOMAIN + '/icons/logo.png'}
+        />
+      </Head>
 
-        <div className={'row'}>
-          <div className={'col'}>
-            <h2 className={'mb-0'}>
-              Currently popular <Link href={'/libraries'}>libraries</Link>
-            </h2>
-            <div className={'mb-3 text-muted'}>
-              According to recent view counts
-            </div>
-          </div>
-
-          <div className={'col-auto'}>
-            <Link href={'/libraries'}>
-              <a className={'btn btn-primary'}>View all</a>
-            </Link>
+      <div className={'row'}>
+        <div className={'col'}>
+          <h2 className={'mb-0'}>
+            Currently popular <Link href={'/libraries'}>libraries</Link>
+          </h2>
+          <div className={'mb-3 text-muted'}>
+            According to recent view counts
           </div>
         </div>
-        <div
-            className={'d-flex flex-wrap mb-4'}
-            style={{ marginRight: '-0.5rem' }}
-        >
-          {libraries.map((library) => {
-            return <LibraryCard library={library} key={library._id} />
-          })}
+
+        <div className={'col-auto'}>
+          <Link href={'/libraries'}>
+            <a className={'btn btn-primary'}>View all</a>
+          </Link>
         </div>
+      </div>
+      <div
+        className={'d-flex flex-wrap mb-4'}
+        style={{ marginRight: '-0.5rem' }}
+      >
+        {libraries.map((library) => {
+          return <LibraryCard library={library} key={library._id} />
+        })}
+      </div>
 
-        <div className={'row'}>
-          <div className={'col'}>
-            <h2 className={'mb-0'}>
-              Currently popular <Link href={'/items'}>items</Link>
-            </h2>
-            <div className={'mb-3 text-muted'}>
-              According to recent view counts
-            </div>
-          </div>
-
-          <div className={'col-auto'}>
-            <Link href={'/items'}>
-              <a className={'btn btn-primary'}>View all</a>
-            </Link>
-          </div>
-        </div>
-        <div
-            className={'d-flex flex-wrap mb-4'}
-            style={{ marginRight: '-0.5rem' }}
-        >
-          {items.map((item) => {
-            return <ItemCard item={item} key={item._id} />
-          })}
-        </div>
-
-        <div className={'row'}>
-          <div className={'col'}>
-            <h2 className={'mb-0'}>
-              Currently popular <Link href={'/collections'}>collections</Link>
-            </h2>
-            <div className={'mb-3 text-muted'}>
-              According to recent view counts
-            </div>
-          </div>
-
-          <div className={'col-auto'}>
-            <Link href={'/collections'}>
-              <a className={'btn btn-primary'}>View all</a>
-            </Link>
+      <div className={'row'}>
+        <div className={'col'}>
+          <h2 className={'mb-0'}>
+            Currently popular <Link href={'/items'}>items</Link>
+          </h2>
+          <div className={'mb-3 text-muted'}>
+            According to recent view counts
           </div>
         </div>
-        <div
-            className={'d-flex flex-wrap mb-4'}
-            style={{ marginRight: '-0.5rem' }}
-        >
-          {collections.map((collection) => {
-            return <CollectionCard collection={collection} key={collection._id} />
-          })}
+
+        <div className={'col-auto'}>
+          <Link href={'/items'}>
+            <a className={'btn btn-primary'}>View all</a>
+          </Link>
+        </div>
+      </div>
+      <div
+        className={'d-flex flex-wrap mb-4'}
+        style={{ marginRight: '-0.5rem' }}
+      >
+        {items.map((item) => {
+          return <ItemCard item={item} key={item._id} />
+        })}
+      </div>
+
+      <div className={'row'}>
+        <div className={'col'}>
+          <h2 className={'mb-0'}>
+            Currently popular <Link href={'/collections'}>collections</Link>
+          </h2>
+          <div className={'mb-3 text-muted'}>
+            According to recent view counts
+          </div>
         </div>
 
-        <div className={'row'}>
-          <div className={'col'}>
-            <h2 className={'mb-0'}>
-              Currently popular <Link href={'/lists'}>lists</Link>
-            </h2>
-            <div className={'mb-3 text-muted'}>
-              According to recent view counts
-            </div>
-          </div>
+        <div className={'col-auto'}>
+          <Link href={'/collections'}>
+            <a className={'btn btn-primary'}>View all</a>
+          </Link>
+        </div>
+      </div>
+      <div
+        className={'d-flex flex-wrap mb-4'}
+        style={{ marginRight: '-0.5rem' }}
+      >
+        {collections.map((collection) => {
+          return <CollectionCard collection={collection} key={collection._id} />
+        })}
+      </div>
 
-          <div className={'col-auto'}>
-            <Link href={'/lists'}>
-              <a className={'btn btn-primary'}>View all</a>
-            </Link>
+      <div className={'row'}>
+        <div className={'col'}>
+          <h2 className={'mb-0'}>
+            Currently popular <Link href={'/lists'}>lists</Link>
+          </h2>
+          <div className={'mb-3 text-muted'}>
+            According to recent view counts
           </div>
         </div>
-        <div
-            className={'d-flex flex-wrap mb-4'}
-            style={{ marginRight: '-0.5rem' }}
-        >
-          {lists.map((list) => {
-            return <ListCard list={list} key={list._id} />
-          })}
+
+        <div className={'col-auto'}>
+          <Link href={'/lists'}>
+            <a className={'btn btn-primary'}>View all</a>
+          </Link>
         </div>
-      </>
+      </div>
+      <div
+        className={'d-flex flex-wrap mb-4'}
+        style={{ marginRight: '-0.5rem' }}
+      >
+        {lists.map((list) => {
+          return <ListCard list={list} key={list._id} />
+        })}
+      </div>
+    </>
   )
 }
 
