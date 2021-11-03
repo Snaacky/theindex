f8347de5b183bd00b9a5cffb6bddee9cb0e6742f
Yasamato <agent77326@gmail.com>
2021-11-03 11:33:55 +0100

    Handle array ips returned

--- pages/api/ip-info.ts
+++ pages/api/ip-info.ts
@@ -1,7 +1,11 @@
 import { lookup } from 'geoip-lite'
 
 export default async function handler(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  if (Array.isArray(ip)) {
+    ip = ip[0]
+  }
+
   const geo = lookup(ip)
   res.status(200).json({
     ip,
