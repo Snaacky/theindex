60ca3b59bf6f2ad2b343952dc9923bc264bfb682
Yasamato <agent77326@gmail.com>
2021-11-12 16:52:55 +0100

    Add cache header to /api/libraries

--- pages/api/libraries.js
+++ pages/api/libraries.js
@@ -1,5 +1,9 @@
 import { getLibrariesWithCollections } from '../../lib/db/libraries'
 
 export default async function handler(req, res) {
+  res.setHeader(
+      'Cache-Control',
+      'public, max-age=60, s-maxage=60, stale-while-revalidate=59'
+  )
   res.status(200).json(await getLibrariesWithCollections())
 }
