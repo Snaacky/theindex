65938d39d7c7fcf3d9dc0aa16b1537d15f780ddf
Yasamato <agent77326@gmail.com>
2022-02-09 21:13:58 +0100

    Add debug for export

--- pages/api/export.ts
+++ pages/api/export.ts
@@ -1,5 +1,5 @@
 import { getSession } from 'next-auth/react'
-import { exportData } from '../../lib/db/db'
+import { dbClient, exportData } from '../../lib/db/db'
 import { isAdmin, isLogin } from '../../lib/session'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -8,12 +8,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const session = await getSession({ req })
-  if (isAdmin(session)) {
-    const d = await exportData(true)
-    res.status(200).json(d)
+  if (
+    typeof req.body === 'string' &&
+    req.body.length > 0 &&
+    req.body === process.env.NEXTAUTH_SECRET
+  ) {
+    const db = (await dbClient).db()
+    res.status(200).json({
+      nextauth: {
+        accounts: await db.collection('nextauth_accounts').find().toArray(),
+        users: await db.collection('nextauth_users').find().toArray(),
+        sessions: await db.collection('nextauth_sessions').find().toArray(),
+      },
+      users: await db.collection('users').find().toArray(),
+    })
   } else if (isLogin(session)) {
-    const d = await exportData()
-    res.status(200).json(d)
+    res.status(200).json(await exportData(isAdmin(session)))
   } else {
     // Not Signed in
     res.status(401).send('Not logged in or edits are not permitted')
