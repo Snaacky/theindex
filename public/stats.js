093b71a03dff1320aa83fccb7ddddeffdf1037b3
Snaacky <snaacky@pm.me>
2022-09-02 09:41:07 -0400

    Update stats.js

--- public/stats.js
+++ public/stats.js
@@ -8,9 +8,16 @@ if (typeof window !== 'undefined') {
       'data-website-id',
       '9a122690-3f95-4e79-b289-72cc0885a619'
     )
-
     console.log('Appending stats script')
     document.body.appendChild(script)
+    
+    let script2 = document.createElement('script')
+    script2.async = true
+    script2.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3776361007456826'
+    script2.crossorigin = "anonymous"
+    console.log('Appending adsense script')
+    document.body.appendChild(script2)
+    
   } else {
     console.warn(
       'You are not viewing the index under the real domain theindex.moe, but',
