b0f1fa8b9cdba2f9526535d47e30227bc0e598c7
Yasamato <agent77326@gmail.com>
2024-12-09 12:40:41 +0100

    Use example from authjs.dev (#118)

---
+++ lib/db/authDbClient.ts
@@ -0,0 +1,37 @@
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Invalid/Missing environment variable: "DATABASE_URL"')
+}
+
+const uri = process.env.DATABASE_URL
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+}
+
+let client: MongoClient
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient
+  }
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options)
+  }
+  client = globalWithMongo._mongoClient
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+}
+
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+export default client
